b724df71a17bc96b57d656e11853f1949fb9670b
hafsah <hafsah>
2026-09-05 20:46:31 +0800

    Activity 4: Interactive Profile

--- hello.py
+++ hello.py
@@ -1,21 +1,34 @@
+#==============Hardcoded Values==============
+
+# print('========================')
+# print('Welcome here')
+# print('My first post')
+# print('========================')
+
+# username = "cool_creator"
+# bio = "Fun Blogger"
+# Followers = 100
+
+# Followers += 50
+# print("Day 1:", Followers)
+
+# Followers += 20
+# print("Day 2:", Followers)
+
+# Followers -= 10
+# print("Day 3:", Followers)
+
+# print('Username:', username)
+# print('Bio:', bio)
+# print('Followers:', Followers)
+
+#==============Interactive Profile==============
+username = input("Enter Username: ")
+age = input("Enter Age: ")
+category = input("Enter Content Category: ")
+
+print('\nInstagram Profile')
 print('========================')
-print('Welcome here')
-print('My first post')
-print('========================')
-
-username = "cool_creator"
-bio = "Fun Blogger"
-Followers = 100
-
-Followers += 50
-print("Day 1:", Followers)
-
-Followers += 20
-print("Day 2:", Followers)
-
-Followers -= 10
-print("Day 3:", Followers)
-
 print('Username:', username)
-print('Bio:', bio)
-print('Followers:', Followers)
+print('Age:', age)
+print('Category:', category)
