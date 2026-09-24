2681ea49e5b49e8ac87c4183d540f6ad4130963c
hasfahh <hafsahmariam06@gmail.com>
2026-09-24 22:09:47 +0800

    Add input validation function

--- Lab_3/modular_auditor.py
+++ Lab_3/modular_auditor.py
@@ -2,60 +2,26 @@ inventory = 0
 failed_entries = 0
 
 
-while True:
-    stock_input = input("Enter stock quantity (or 'quit' to exit): ").strip()
-
-    # Check if user wants to quit
-    if stock_input.lower() == "quit":
-        print("Total Units Processed:", inventory)
-        print("Number of Failed/Rejected Entries:", failed_entries)
-        break
-
-    # Check for negative numbers
-    if stock_input.startswith("-") and stock_input[1:].isdigit():
-        print("Error: Stock quantity cannot be negative.")
-        failed_entries += 1
-        continue
-
-    # Check for invalid input
-    if not stock_input.isdigit():
-        print("Error: Please enter a valid integer.")
-        failed_entries += 1
-        continue
-
-    # Convert valid input to integer
-    stock = int(stock_input)
-
-     # Add stock to inventory
-    inventory += stock
-    print("Current Inventory:", inventory)
-
-    # Check for overstock
-    if inventory > 500:
-        print("ALERT: Inventory has exceeded maximum capacity of 500 units!")
-        break
-    elif inventory >= 400:
-        print("WARNING: inventory is nearing maximum capacity:")
-
 def get_valid_input():
     while True:
-        user_input = input("Enter stock quantity or 'quit': ")
+        stock_input = input("Enter stock quantity (or 'quit' to exit): ").strip()
 
-        if user_input.lower() == "quit":
+        if stock_input.lower() == "quit":
             return "quit"
 
-        try:
-            quantity = int(user_input)
+        # Check for negative numbers
+        if stock_input.startswith("-") and stock_input[1:].isdigit():
+            print("Error: Stock quantity cannot be negative.")
+            continue
 
-            if quantity < 0:
-                print("Stock quantity cannot be negative.")
-                continue
+        # Check for invalid input
+        if not stock_input.isdigit():
+            print("Error: Please enter a valid integer.")
+            continue
 
-            return quantity
+        # Convert valid input to integer
+        return int(stock_input)
 
-        except ValueError:
-            print("Invalid input. Please enter a whole number.")
-            
+
 value = get_valid_input()
 print(value)
-
