3a76aaccc810b39523f1db4fa35fb1e9a1de506b
hasfahh <hafsahmariam06@gmail.com>
2026-09-24 22:13:05 +0800

    Add tax calculation function

--- Lab_3/modular_auditor.py
+++ Lab_3/modular_auditor.py
@@ -26,4 +26,8 @@ def process_delivery(current_total, new_value):
     new_total = current_total + new_value
     return new_total
 
-print(process_delivery(100, 50))
+def calculate_tax(amount):
+    tax = amount * 0.10
+    return tax
+
+print(calculate_tax(50))
